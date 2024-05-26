247b8b1f55fbbcfe665e6bbc2383e063af6eb727
Akshay <103642286+Akshay1495@users.noreply.github.com>
2024-05-26 07:23:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function getFormvalue() {
     //Write your code here
 var firstName = document.getElementById('form1').elements['fname'].value;
     var lastName = document.getElementById('form1').elements['lname'].value;
-    alert("First Name: " + firstName + "\nLast Name: " + lastName);
+    alert(firstName +' '+ lastName);
     return false;
 }
